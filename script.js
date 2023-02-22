9074b5be51ab7e28432c0c6554e585ee1647f9cc
Raul_Navarro_Sos <raulnsos@gmail.com>
2023-02-22 13:12:48 +0100

    if else antes de refactorizar

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ const numberField = document.querySelector(".number");
 
 // 2do paso: crear las variables que necesitamos
 
-const score = 20;
+let score = 20;
 const highscore = 0;
 
 // obtener un numero aleatorio entre 1 y 20
@@ -19,6 +19,26 @@ const secretNumber = Math.trunc(Math.random() * MAX_NUMBER) + MIN_NUMBER;
 
 const number = numberField.value;
 
-// 3er paso: crear las funciones que necesitemos
 console.log(`El numero ingresado es ${secretNumber}`);
 console.log(`El numero ingresado es ${number}`);
+
+// 3er paso: añadir un listener al checkButton y mostrar un mensaje que
+// diga si el numero es mayor o menos en el campo messageField
+
+CheckButton.addEventListener("click", function () {
+  const number = guessField.value;
+  console.log(`El numero ingresado es ${number}`);
+  if (number < secretNumber) {
+    messageField.textContent = "El numero es mayor";
+    score = score - 1;
+    scoreField.textContent = score;
+  } else if (number > secretNumber) {
+    messageField.textContent = "El numero es menor";
+    score--;
+    scoreField.textContent = score;
+  } else {
+    messageField.textContent = "Ganaste";
+  }
+});
+
+//4to paso:
