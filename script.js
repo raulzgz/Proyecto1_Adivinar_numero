998c6d13f73a73651b2f252a2c8ed6726121fdca
Raul_Navarro_Sos <raulnsos@gmail.com>
2023-02-22 12:24:11 +0100

    conexion

--- script.js
+++ script.js
@@ -1,27 +1,36 @@
-const scoreField = document.querySelector('.score')
-const CheckButton = document.querySelector('.check')
-const againButton = document.querySelector('.again')
-const guessField = document.querySelector('.guess')
-const highscoreField = document.querySelector('.highscore')
-const messageField = document.querySelector('.message')
-const numberField = document.querySelector('.number')
+// 1er paso: seleccionar loes elementos del DOM y guardarlos en variables
+const scoreField = document.querySelector(".score");
+const CheckButton = document.querySelector(".check");
+const againButton = document.querySelector(".again");
+const guessField = document.querySelector(".guess");
+const highscoreField = document.querySelector(".highscore");
+const messageField = document.querySelector(".message");
+const numberField = document.querySelector(".number");
 
 console.log(
-    scoreField,
-    CheckButton,
-    againButton,
-    guessField,
-    highscoreField,
-    messageField,
-    numberField
-)
+  scoreField,
+  CheckButton,
+  againButton,
+  guessField,
+  highscoreField,
+  messageField,
+  numberField
+);
+// 2do paso: crear las variables que necesitamos
 
-scoreField.textContent = 10
+const score = 20;
+const highscore = 0;
+const secretNumbre = Math.trunc(Math.random() * 20) + 1;
+const number = numberField.value;
 
-CheckButton.addEventListener('click', function(){
-    alert ('Hola mundo')
-})
+// 3er paso: crear las funciones que necesitemos
 
-function alert(){
-    alert ('Me hiciste click')
+scoreField.textContent = 10;
+
+CheckButton.addEventListener("click", function () {
+  alert("Hola mundo");
+});
+
+function alert() {
+  alert("Me hiciste click");
 }
