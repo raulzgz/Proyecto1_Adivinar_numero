53ce2cd268fb46df95a6ab881f5c2938528f569d
Raul_Navarro_Sos <raulnsos@gmail.com>
2023-02-22 14:23:30 +0100

    Finalizado
    Corregidos errores

--- script.js
+++ script.js
@@ -16,12 +16,13 @@ let highscore = 0;
 // obtener un numero aleatorio entre 1 y 20
 const MIN_NUMBER = 1;
 const MAX_NUMBER = 20;
-const secretNumber = Math.trunc(Math.random() * MAX_NUMBER) + MIN_NUMBER;
+let secretNumber;
 
-const number = numberField.value;
+fnInitApp();
+
+const number = Number(guessField.value);
 
 console.log(`El numero ingresado es ${secretNumber}`);
-console.log(`El numero ingresado es ${number}`);
 
 // 3er paso: añadir un listener al checkButton y mostrar un mensaje que
 // diga si el numero es mayor o menos en el campo messageField
@@ -48,7 +49,8 @@ CheckButton.addEventListener("click", function () {
     numberField.textContent = secretNumber;
   } else if (score === 1) {
     mostrarMensaje("Perdiste");
-    bodyField.style.backgroundColor = "red";
+    scoreField.textContent = 0;
+    bodyField.style.backgroundColor = "#b34747";
   } else {
     messageField.textContent =
       number > secretNumber ? "El numero es menor" : "El numero es mayor";
@@ -74,3 +76,16 @@ CheckButton.addEventListener("click", function () {
   }
 });
 */
+
+againButton.addEventListener("click", fnInitApp);
+
+function fnInitApp() {
+  score = 20;
+  scoreField.textContent = score;
+  guessField.value = "";
+  secretNumber = Math.trunc(Math.random() * MAX_NUMBER) + MIN_NUMBER;
+  console.log(`El número secreto es ${secretNumber}`);
+  mostrarMensaje("Empieza de nuevo");
+  bodyField.style.backgroundColor = "#222";
+  numberField.textContent = "?";
+}
